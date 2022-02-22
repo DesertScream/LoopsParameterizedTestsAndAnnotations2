d240eaac619060ff4f1540e54e6c7c4ac9a1701a
DesertScream <www.anton.dzuin@gmail.com>
2022-02-23 03:29:49 +0500

    twoiter

--- src/test/java/ru/netology/stats/StatsService.java
+++ src/test/java/ru/netology/stats/StatsService.java
@@ -6,11 +6,11 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class StatsService {
     @Test
-    void shouldSummaSails() {
+    void summaSails() {
         StatsService service = new StatsService();
         int[] salesByMonth = {8, 15, 13, 15, 17, 20, 19, 20, 7, 14, 14, 18};
         int expected = 180;
-        int actual = service.shouldSummaSails(salesByMonth);
+        int actual = service.summaSails(salesByMonth);
         assertEquals(expected, actual);
 
     }
